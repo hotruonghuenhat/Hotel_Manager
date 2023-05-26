cf1d8d8c78462e1f3854c981ed1609a4c615f846
hotruonghuenhat <hotruonghuenhat@gmail.com>
2023-05-26 17:06:18 +0700

    update

--- Hotel-Manager/TatBlog.WebApi/Models/ValidationFailureResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TatBlog.WebApi.Models;
-
-public class ValidationFailureResponse {
-    public IEnumerable<string> Errors { get; set; }
-
-    public ValidationFailureResponse(IEnumerable<string> errors) {
-        Errors = errors;
-    }
-}
